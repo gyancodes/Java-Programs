cf6bdf2fcd65abe31370d1b4cc1c3d556770d953
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2022-09-03 21:04:03 +0530

    add: CheckEvenOdd.java

--- CheckEvenOdd.java
+++ CheckEvenOdd.java
@@ -3,7 +3,18 @@ import java.util.Scanner;
 class CheckEvenOdd{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Number: ");
         
+        int num = sc.nextInt();
+
+
+        if(num % 2 == 0){
+            System.out.println("Even Number");
+        }else{
+            System.out.println("Odd Number");
+        }
+
 
 
     }
