efdf5affa39f2f7785847ced8d2cb599e93468be
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2022-09-06 20:40:48 +0530

    fix

---
+++ CalculateValue.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CalculateValue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+        System.out.print("Enter First Number: ");
+        int firstNumber = sc.nextInt();
+
+
+        System.out.print("Enter Second Number: ");
+        int secondNumber = sc.nextInt();
+
+        if(1)
+    }
+}
