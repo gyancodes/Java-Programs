a19368832699cd7d36f3f1d683b0b52fdc3cc57a
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2022-09-21 22:12:38 +0530

    PrintLargestNumber

---
+++ PrintLargestNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PrintLargestNumber {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter First Number: ");
+        int num1 = in.nextInt();
+
+        System.out.print("Enter Second Number: ");
+        int num2 = in.nextInt();
+
+        if(num1 > num2){
+            System.out.println("Greater: " + num1);
+        }
+        else if(num2 > num1) {
+            System.out.println("Greater: " + num2);
+        }
+        else{
+            System.out.println("Both are Equal10");
+        }
+
+        
+    }
+}
