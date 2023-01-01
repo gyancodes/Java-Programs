9baf423f999c47fcf0b9c556bab0a848211ad760
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2023-01-01 23:15:03 +0530

    sumfunc()

--- Array.java
+++ Array.java
@@ -13,6 +13,6 @@ public class Array{
             System.out.println(arr[i]);
         }
 
-
+        
     }
 }
---
+++ Function.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Function {
+    public static void main(String[] args) {
+        sum();
+
+    }
+
+   static void sum() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter 1st Number: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter 2nd Number: ");
+        int num2 = in.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+    }
+}
+
+// 2 Methods to print the maximum and the minimum number respectively among
+// three numbers entered by the user
