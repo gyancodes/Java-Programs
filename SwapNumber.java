603666533ad0d05db519f1d861fee71d0f52542f
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2023-01-01 23:52:15 +0530

    SwapNumbers

---
+++ SwapNumber.java
@@ -0,0 +1,12 @@
+public class SwapNumber {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 40;
+
+        int temp = a;
+        a=b;
+        b= temp;
+
+        System.out.println(a + " " +b);
+    }
+}
